d4d4b024944c1dcde148bb81ea60aa85415273ba
Garret Jennings <gjennings@me.com>
2022-10-23 02:09:30 -0700

    Comment unused

--- lua/config/illuminate.lua
+++ lua/config/illuminate.lua
@@ -3,7 +3,7 @@ if not status_ok then
     return
 end
 
-local cmd = vim.cmd
+-- local cmd = vim.cmd
 
 illuminate.configure({
     -- providers: provider used to get references in the buffer, ordered by priority
