cef18e921168f413b0535cd76d471076b9842ddc
Garret Jennings <gjennings@me.com>
2023-10-04 23:28:58 -0700

    Update indent-blankline config

--- lua/config/indent-blankline.lua
+++ lua/config/indent-blankline.lua
@@ -1,57 +1,57 @@
-local status_ok, indent = pcall(require, "indent_blankline")
+local status_ok, indent = pcall(require, "ibl")
 if not status_ok then
     return
 end
 
 indent.setup({
-    indentLine_enabled = 1,
-    filetype_exclude = {
-        "NvimTree",
-        "TelescopePrompt",
-        "dashboard",
-        "dotooagenda",
-        "flutterToolsOutline",
-        "fugitive",
-        "git",
-        "gitcommit",
-        "help",
-        "log",
-        "markdown",
-        "packer",
-        "peekaboo",
-        "startify",
-        "todoist",
-        "txt",
-        "undotree",
-        "vim",
-        "vimwiki",
-        "vista",
-        -- for all buffers without a file type
-        "",
+    enabled = true,
+    scope = {
+        enabled = true,
+        char = "┃",
+        show_start = false,
+        show_end = false,
+        highlight = { "Function", "Label" },
     },
-    char = "│",
-    -- char_list = {"|", "¦", "┆", "┊"},
-    buftype_exclude = { "terminal", "nofile", "dashboard" },
-    show_trailing_blankline_indent = false,
-    show_first_indent_level = false,
-    show_current_context = true,
-    show_current_context_start = false,
-    space_char = " ",
-    space_char_blankline = " ",
-    -- char_highlight_list = {
-    --     "IndentBlanklineIndent",
-    -- },
-    -- context_patterns = {
-    --     "^if",
-    --     "^table",
-    --     "block",
-    --     "class",
-    --     "for",
-    --     "function",
-    --     "if_statement",
-    --     "list_literal",
-    --     "method",
-    --     "selector",
-    --     "while"
-    -- }
+    exclude = {
+        filetypes = {
+            "",
+            "NvimTree",
+            "TelescopePrompt",
+            "TelescopeResults",
+            "checkhealth",
+            "dashboard",
+            "dotooagenda",
+            "flutterToolsOutline",
+            "fugitive",
+            "git",
+            "gitcommit",
+            "help",
+            "log",
+            "lspinfo",
+            "man",
+            "markdown",
+            "packer",
+            "peekaboo",
+            "startify",
+            "todoist",
+            "txt",
+            "undotree",
+            "vim",
+            "vimwiki",
+            "vista",
+            -- for all buffers without a file type
+        },
+        buftypes = {
+            "terminal",
+            "nofile",
+            "dashboard",
+            "quickfix",
+            "prompt",
+        },
+    },
+    indent = {
+        -- char = {"|", "¦", "┆", "┊"},
+        char = "│",
+        tab_char = "├",
+    },
 })
