69c202fd07fa40d70cc194f4d08a63fa220b485c
Garret Jennings <gjennings@me.com>
2023-10-04 23:32:13 -0700

    fix: update telescope selection character

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -85,7 +85,7 @@ telescope.setup({
             },
         },
         prompt_prefix = " ❯ ",
-        selection_caret = " ",
+        selection_caret = "󰋇 ",
         entry_prefix = "  ",
         initial_mode = "insert",
         selection_strategy = "reset",
